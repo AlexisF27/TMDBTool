495b48027544be212fde62903353f1eadc4831b7
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-12-16 17:12:00 +0000

    Project base ready main cpp

---
+++ include/TMDBTool/TMDBTool.h
@@ -0,0 +1,15 @@
+#ifndef TMDBTOOL_H
+#define TMDBTOOL_H
+
+#include <string>
+
+using namespace std;
+
+class TMDBTool
+{
+public:
+  TMDBTool();
+  ~TMDBTool();
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,45 @@
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include "../include/TMDBTool/TMDBTool.h"
+
+using namespace std;
+
 int main(int argc, char* argv[])
 {
-  int require = 0;
+  try
+  {
+    if (argc != 3)
+      {
+        cerr << "Error: Invalid command line arguments.\n";
+        cerr << "Usage: tmdb-app --type <type>\n";
+        return 1;
+      }
+
+    string argType = argv[1];
+    if (argType != "--type")
+    {
+      cerr << "Error: Missing '--type' argument.\n";
+      cerr << "Usage: tmdb-app --type <type>\n";
+      return 2;
+    }
+
+    string type = argv[2];
+
+    if (type == "playing" || type == "popular" || type == "top" || type == "upcoming")
+    {
+      /* TODO */
+    }else{
+      cerr << "Error: Invalid type.\n";
+      cerr << "Usage: tmdb-app --type <type>\n";
+      cerr << "Types: playing, popular, top, upcoming\n";
+      return 3;
+    }
+    
+    
+  }
+  catch(const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+  }
+  
 }
